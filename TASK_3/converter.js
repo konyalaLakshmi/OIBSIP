d970ae92af0435ed4c3ffe864f94fb47a8ccaaea
konyalaLakshmi <lakshmikonyala919@gmail.com>
2024-02-23 22:11:03 +0530

    landing

--- TASK_3/converter.js
+++ TASK_3/converter.js
@@ -12,15 +12,15 @@ function convertTemperature() {
     var convertedTemperature;
     if (selectUnit.value === "celsius") {
         convertedTemperature = (parseFloat(inputElement.value) * 9 / 5) + 32; // Celsius to Fahrenheit
-        units = "Fahrenheits"
+        units = "°F";
     } else if (selectUnit.value === "fahrenheit") {
         convertedTemperature = (parseFloat(inputElement.value) - 32) * 5 / 9; // Fahrenheit to Celsius
-        units = "Celsius"
+        units = "°C";
     } else if (selectUnit.value === "kelvin") {
-        convertedTemperature = (parseFloat(inputElement.value) - 273.15) //Kelvin to Celsius
-        units = "Celsius"
+        convertedTemperature = parseFloat(inputElement.value) - 273.15; // Kelvin to Celsius
+        units = "K";
     }
 
-    let spanElement = document.getElementById("span")
-    spanElement.textContent = convertedTemperature.toFixed(2) + " " + units
+    let spanElement = document.getElementById("span");
+    spanElement.textContent = convertedTemperature.toFixed(2) + " " + units;
 }
